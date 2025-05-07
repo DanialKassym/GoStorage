a8bf327031f614e67ea38e48d79e5c78bad3ff7c
Danial Kassym <danialkassym@gmail.com>
2025-05-08 00:08:52 +0500

    Added basic filetype detection to ignore requests with unsupported file extensions

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -12,6 +12,7 @@ import (
 	db "github.com/DanialKassym/GoStorage/internal/Database"
 )
 
+// Used for db connection testing
 func RetriveUsers(w http.ResponseWriter, r *http.Request) {
 
 	ret := db.GetAllUsers()
@@ -21,15 +22,15 @@ func RetriveUsers(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(ret)
 }
 
+// supports pdf and text files
 func UploadObject(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	r.ParseMultipartForm(512 << 20)
 	fmt.Println("Content-Type:", r.Header.Get("Content-Type"))
-	var buf bytes.Buffer
-	fmt.Println("Before FormFile")
+
 	file, header, err := r.FormFile("file")
 	if err != nil {
-		http.Error(w, "Error retrieving the file: "+ err.Error(), http.StatusBadRequest)
+		http.Error(w, "Error retrieving the file: ", http.StatusBadRequest)
 		return
 	}
 	defer file.Close()
@@ -37,11 +38,20 @@ func UploadObject(w http.ResponseWriter, r *http.Request) {
 	name := strings.Split(header.Filename, ".")
 	fmt.Printf("File name is: %s\n", name[0])
 
+	var buf bytes.Buffer
 	n, _ := io.Copy(&buf, file)
 	fmt.Printf("Read %d bytes\n", n)
+
+	// Reading the first 512 bytes to detect content type
+	contentType := http.DetectContentType(buf.Bytes()[:512])
+	fmt.Println("Detected content type:", contentType)
+
+	if contentType != "application/pdf" && contentType != "application/octet-stream" {
+		http.Error(w, "Unsupported content type: ", http.StatusBadRequest)
+		return
+	}
 	contents := buf.String()
 	fmt.Println(contents)
-
 	f, _ := os.Create("/tmp/data")
 	defer f.Close()
 	n2, _ := f.Write([]byte(contents))
