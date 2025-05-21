8e4469e2537d808941be3613acd10647944fd555
Danial Kassym <danialkassym@gmail.com>
2025-05-21 20:10:15 +0500

    Docker and Docker compose deployment is implemented

---
+++ cmd/authentication/go.mod
@@ -0,0 +1,8 @@
+module github.com/DanialKassym/GoStorage/cmd/authentication
+
+go 1.24.1
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.2
+	github.com/joho/godotenv v1.5.1
+)
--- cmd/authentication/main.go
+++ cmd/authentication/main.go
@@ -1,2 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	authentication "github.com/DanialKassym/GoStorage/cmd/authentication/auth"
+)
+
+func main(){
+	fmt.Println("hello")
+	authentication.GenerateJWT()
+	
+}
